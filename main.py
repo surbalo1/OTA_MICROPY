ee55507584df3ec12d7de1d65cbd63f58a8b277e
surbalo1 <54747170+surbalo1@users.noreply.github.com>
2024-06-27 15:07:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
+from machine import Pin
+import time
+
+# Define the pin where the LED is connected
+led = Pin(2, Pin.OUT)
+
+while True:
+    # Turn on the LED
+    led.on()
+    # Wait for 2 seconds
+    time.sleep(2)
+    # Turn off the LED
+    led.off()
+    # Wait for 2 seconds
+    time.sleep(2)
